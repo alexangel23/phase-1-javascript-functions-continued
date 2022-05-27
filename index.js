24a30cc3ec096988a077329f24537f909a5c9dc9
Alexangel Sanchez <alexangel2323@gmail.com>
2022-05-26 19:07:34 -0500

    commit tree ef49932e455029855a1836ba518bf0269ac82b8b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Implement a function called saturdayFun . It should return a string
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
